75d6120a6bbd72731bcb47f8890a2acd4e226f9e
youroldmangaming <youroldmangaming@gmail.com>
2024-10-27 18:55:41 +1300

    Create time.sh

---
+++ time.sh
@@ -0,0 +1,19 @@
+sudo apt-get install chrony
+
+echo "confdir /etc/chrony/conf.d
+sourcedir /etc/chrony/sources.d
+keyfile /etc/chrony/chrony.keys
+driftfile /var/lib/chrony/chrony.drift
+ntsdumpdir /var/lib/chrony
+logdir /var/log/chrony
+maxupdateskew 100.0
+rtcsync
+makestep 1 3
+leapsectz right/UTC
+server 192.168.188.141 iburst" >/etc/chrony/chrony.conf
+
+sudo systemctl enable chrony
+sudo systemctl start chrony
+
+chronyc sources
+chronyc tracking
