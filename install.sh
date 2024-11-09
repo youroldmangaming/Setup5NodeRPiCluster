f7d31ffff135fe0a247d74761026be0efdd57293
youroldmangaming <youroldmangaming@gmail.com>
2024-11-10 03:26:56 +1300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Variables
-REPO_URL="https://github.com/youroldmangaming/GlusterSetup5NodeRPiCluster.git" # Change this to your repository URL
+REPO_URL="https://github.com/youroldmangaming/Setup5NodeRPiCluster.git" # Change this to your repository URL
 REPO_DIR="Setup" # Change this to your desired directory name if different
 ENV_FILE="../.env"
 
@@ -20,11 +20,11 @@ cd "$REPO_DIR" || { echo "Failed to enter directory: $REPO_DIR"; exit 1; }
 # Step 3: Execute your scripts
 # Step 3: Create .env file
 
-if [ -f "$ENV_FILE" ]; then
+#if [ -f "$ENV_FILE" ]; then
 
-    rm "$ENV_FILE" # Remove existing .env file
+#    rm "$ENV_FILE" # Remove existing .env file
 
-fi
+#fi
 
  
 
@@ -46,56 +46,56 @@ fi
 
 # Replace script1.sh and script2.sh with your actual script names
 
-if [ -f host.sh ]; then
+#if [ -f host.sh ]; then
 
-    chmod +x host.sh
+#    chmod +x host.sh
 
-    ./host.sh
+#    ./host.sh
 
-else
+#else
 
-    echo "host.sh not found"
-fi
+#    echo "host.sh not found"
+#fi
 
 
  
 
-if [ -f share.sh ]; then
+#if [ -f share.sh ]; then
 
-    chmod +x share.sh
+ #   chmod +x share.sh
 
-    ./share.sh
+  #  ./share.sh
 
-else
+#else
 
-    echo "share.sh not found"
+ #   echo "share.sh not found"
 
-fi
+#fi
 
 
-if [ -f slurm.sh ]; then
+#if [ -f slurm.sh ]; then
 
-    chmod +x slurm.sh
+ #   chmod +x slurm.sh
 
-    ./slurm.sh
+  #  ./slurm.sh
 
-else
+#else
 
-    echo "slurm.sh not found"
+ #   echo "slurm.sh not found"
 
-fi
+#fi
 
 
 
 # Step 4: Exit the repository directory
 
-cd .. || exit 1
+#cd .. || exit 1
 
  
 
 # Step 5: Remove the cloned repository
 
-rm -rf "$REPO_DIR"
+#rm -rf "$REPO_DIR"
 
  
 
